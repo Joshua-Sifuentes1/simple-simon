dd64cbccf8347b5ac8927359df7350151f270ce1
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-07 11:26:58 -0600

    Multiple changes since last commit. -Added multiple functions to call when code is running. 	-Start Game function: 		starts the game when the button is pressed and will reset most counters back to default values. 		removes any styling classes before game start up. 		added a fadeIn to show the round number 	-removeClass function: 		Removes style effects not intended to show during the game. 	-Game Over function: 		Added add class function, for styling effects on loss, to game over and resets all values back to defaults. 	-Animate function: 		Added function to animate the simon move. setup interval for each of simons moves according to the sequence. 	-Simon Move function: 		Added animate function. -Variable changes: 	-Added counter, roundNumber and highScore variables. 	-changed colors variable from array to jquery element. -completed click function for game. 	-player can now press buttons after simon sequence is finished. 	-added if else statements to register correct or incorrect inputs for colors pressed. -Added comments and spaced out code.

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
-
-
-	var colors = ["red", "blue", "yellow", "green"];
-
-	var simonSequence = [];
+	"use strict"
 
 // -----------------------------------------------------------------
-//		Functions that will animate the square that is clicked.
+//							Variables
+// -----------------------------------------------------------------
+
+	var colors = $(".square");
+	var simonSequence = [];
+	var counter =  0;
+	var roundNumber = 1;
+	var highScore = 0;
+
+// -----------------------------------------------------------------
+//	Functions that will animate the square that is clicked/pressed.
 // -----------------------------------------------------------------
 
 	function squarePressed(element){
@@ -17,41 +23,63 @@ $(document).ready(function(){
 		}, 200);
 	};
 
-	var counter =  0;
+// -----------------------------------------------------------------
+//		
+// -----------------------------------------------------------------
+
 
 	$(".square").click(function(){
 		squarePressed($(this));
-		console.log($(this).data("color"));
 
-		if($(this).data("color") == simonSequence[counter]){
+		if($(this).data('color') == $(simonSequence[counter]).data('color')){
 
 			if(counter == (simonSequence.length - 1)){
 
 				counter = 0;
-				randomColor();
-				console.log(simonSequence);
+				roundNumber++;
+				$(".round-number").html("Round: " + roundNumber);
+				simonMove();
+				
 			} else {
 				counter++;
-				simonMove();
-				console.log(simonSequence);
+			}
+
+			//-----------------------------------
+			//		  High Score counter
+			//-----------------------------------
+
+			if(highScore < roundNumber) {
+				highScore = roundNumber - 1;
+				$("#high-score").html("High Score: " + highScore);
+			} 
+
+			//-----------------------------------
+			//	Adding box animation at round 5
+			//-----------------------------------
+
+			if(roundNumber >= 5){
+				$(".red-square").addClass("add-animation");
+				$(".blue-square").addClass("add-animation");
+				$(".yellow-square").addClass("add-animation2");
+				$(".green-square").addClass("add-animation2");
 			}
 
 		} else {
 
-			simonSequence = [];
+			gameOver();
 		}
-		console.log(counter);
 
 	});
 
 	squarePressed();
 
 // -----------------------------------------------------------------
-//		
+//							Simon Sequence
 // -----------------------------------------------------------------
 
 	//Random color generator for Simon
 	function randomColor(){
+
 		//Random number generator 0-3
 		var randomNumber = Math.floor((Math.random() * 4));
 		var color = colors[randomNumber];
@@ -60,33 +88,65 @@ $(document).ready(function(){
 
 	
 	function simonMove(){
-		var interval = setInterval(function(){
-			squarePressed(simonSequence);
-
-		});
-
+		randomColor();
+		animate(simonSequence);
 	};
+
+	function animate(sequence) {
+		var i = 0;
+		var interval = setInterval(function() {
+		squarePressed(sequence[i]);
+			
+			i++;
+
+			if (i >= sequence.length) {
+				clearInterval(interval);
+			}
+
+		}, 600);
+	}
+
 // -----------------------------------------------------------------
-//					Start game button
+//						Start game button
 // -----------------------------------------------------------------
 
 	function startGame(){
 		$("#start-button").click(function(){
-			randomColor();
+			simonSequence = [];
+			roundNumber = 1;
+			simonMove();
 			console.log(simonSequence);
+			removeClass();
+			$(".round-number").fadeIn(function() {
+				$(this).html("Round: 1");
+			});
 		});
 	};
 	startGame();
+
 // -----------------------------------------------------------------
-//		
+//						Game Over function
 // -----------------------------------------------------------------
 
 	function gameOver(){
 
+		simonSequence = [];
+		counter = 0;
+		roundNumber = 1;
+		$(".round-number").html("Game Over! Press 'Start Game' to play again.");
+		$(".red-square").addClass("red-square-glow");
+		$(".blue-square").addClass("blue-square-glow");
+
 	};
-
-
-
+// -----------------------------------------------------------------
+//						Remove Class function
+// -----------------------------------------------------------------
+	function removeClass(){
+		$(".square").removeClass("add-animation");
+		$(".square").removeClass("add-animation2");
+		$(".square").removeClass("red-square-glow");
+		$(".square").removeClass("blue-square-glow");
+	}
 
 
 });
